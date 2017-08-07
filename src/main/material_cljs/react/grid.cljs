aad7b475830b3b4c690b99c992d1c3af596aa39c
Jeremy Schoffen <jschoffen.dev@gmail.com>
2017-08-07 19:35:58 +0200

    Added specs to grid components and added forgotten options the the grid component.

--- src/main/material_cljs/react/grid.cljs
+++ src/main/material_cljs/react/grid.cljs
@@ -1,26 +1,42 @@
 (ns material-cljs.react.grid
   (:require
+    [clojure.spec.alpha :as s]
     [material-cljs.utils :as u]
     [material-cljs.react-wrapper.core :as w]
     [material-cljs.dom-helpers :as dom]))
 
-;; TODO: Spec components
+;; ---------------------------------------------------------------------------------------------------------------------
+(def grid-props->mdc-classes
+  {:fixed (constantly "mdc-layout-grid--fixed-column-width")
+   :grid-align (fn [p]
+                 (case p
+                   :left "mdc-layout-grid--align-left"
+                   :right "mdc-layout-grid--align-right"
+                   ""))})
 
 (w/def-component Grid
   (render [this]
-          (u/render-container this dom/div {:className "mdc-layout-grid"})))
+    (let [props (w/props this)]
+      (u/render-container this dom/div {:className (u/mdc-classes
+                                                     "mdc-layout-grid"
+                                                     (u/mdc-classes-from-props props grid-props->mdc-classes))
+                                        ::u/props-filter [:grid-align :fixed]}))))
+(s/def ::fixed boolean?)
+(s/def ::grid-align #{:left :right})
 
-(def mdc-grid (w/factory Grid))
+(s/def ::grid-props (s/keys :opt-un [::fixed ::grid-align]))
 
+(w/def-constructor mdc-grid Grid :spec ::grid-props)
 
+;; ---------------------------------------------------------------------------------------------------------------------
 (w/def-component GridInner
   (render [this]
     (u/render-container this dom/div {:className "mdc-layout-grid__inner"})))
 
-(def mdc-grid-inner (w/factory GridInner))
+(w/def-constructor mdc-grid-inner GridInner)
 
 
-
+;; ---------------------------------------------------------------------------------------------------------------------
 (def make-span-class (partial u/make-mdc-class "mdc-layout-grid__cell--span-"))
 (def make-order-class (partial u/make-mdc-class "mdc-layout-grid__cell--order-"))
 (def make-alignment-class (partial u/make-mdc-class "mdc-layout-grid__cell--align-"))
@@ -50,4 +66,13 @@
       (u/render-container this dom/div {:className (cell-classes props)
                                         ::u/props-filter (keys cell-props->mdc-classes)}))))
 
-(def mdc-grid-cell (w/factory GridCell))
+(s/def ::span (s/and int? #(< 0 % 13)))
+(s/def ::span-tablet (s/and int? #(< 0 % 13)))
+(s/def ::span-phone (s/and int? #(< 0 % 13)))
+(s/def ::order (s/and int? #(< 0 % 13)))
+(s/def ::align #{:top :middle :bottom})
+
+(s/def ::grid-cell-props (s/keys :opt-un [::span ::span-phone ::span-tablet ::order ::align]))
+
+
+(w/def-constructor mdc-grid-cell GridCell :spec ::grid-cell-props)
