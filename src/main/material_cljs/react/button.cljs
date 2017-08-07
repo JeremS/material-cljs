421194b9048e3ad5fc0115bd7fdb58c9f9705406
Jeremy Schoffen <jschoffen.dev@gmail.com>
2017-08-07 17:26:13 +0200

    Spec'ted Button props.

--- src/main/material_cljs/react/button.cljs
+++ src/main/material_cljs/react/button.cljs
@@ -1,5 +1,6 @@
 (ns material-cljs.react.button
   (:require
+    [clojure.spec.alpha :as s]
     [material-cljs.react-wrapper.core :as w]
     [material-cljs.utils :as u]
     [material-cljs.dom-helpers :as dom]))
@@ -8,8 +9,18 @@
 
 ;; TODO: adding ripples
 
-(def mdc-button-props [:dense :raised :compact :primary :accented])
 
+
+
+(s/def ::dense boolean?)
+(s/def ::raised boolean?)
+(s/def ::compact boolean?)
+(s/def ::primary boolean?)
+(s/def ::accented boolean?)
+
+(s/def ::mdc-button-props (s/keys :opt-un [::dense ::raised ::compact ::primary ::accented]))
+
+(def mdc-button-props [:dense :raised :compact :primary :accented])
 (def props->mdc-class
   (zipmap mdc-button-props
           (map (partial constantly) ["mdc-button--dense"
@@ -30,4 +41,4 @@
                                  (u/mdc-classes-from-props props props->mdc-class)))
                   (w/children this)))))
 
-(def mdc-button (w/factory Button))
+(w/def-constructor mdc-button Button :spec ::mdc-button-props)
